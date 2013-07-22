193855f2d4a1f2282ccc4f5ca90182b83f4d05c0
Nikko <nikko.rush@gmail.com>
2013-07-21 23:04:09 -0700

    Vector3, Testing project.  Subtract operator for both vectors

--- Vector2.cs
+++ Vector2.cs
@@ -4,27 +4,27 @@ using System.Linq;
 using System.Text;
 
 namespace Aardvark {
-    public struct Vector {
+    public struct Vector2{
         private float x, y;
 
-        public Vector(float i) {
+        public Vector2(float i) {
             this.x = i;
             this.y = i;
         }
-        public Vector(float x, float y) {
+        public Vector2(float x, float y) {
             this.x = x;
             this.y = y;
         }
 
         public float X {
-            get { return this.X; }
+            get { return this.x; }
             set { this.x = value; }
         }
         public float Y {
             get { return this.y; }
             set { this.y = value; }
         }
-        public double Length { get { return Math.Sqrt(this.x + this.y); } }
+        public double Length { get { return Math.Sqrt(this.x*this.x + this.y*this.y); } }
         public double LengthSquared { get { return this.Length * this.Length; } }
 
         /// <summary>
@@ -34,29 +34,33 @@ namespace Aardvark {
             return String.Format("<{0},{1}>", this.x, this.y);
         }
 
-        public static Vector operator +(Vector i, Vector j) {
-            return new Vector(i.x + j.x, i.y + j.y);
+        public static Vector2 operator +(Vector2 i, Vector2 j) {
+            return new Vector2(i.x + j.x, i.y + j.y);
         }
-        public static Vector operator *(Vector v, float x) {
-            return new Vector(v.x * x, v.y * x);
+        public static Vector2 operator -(Vector2 i, Vector2 j) {
+            j.Negate();
+            return i + j ;
         }
-        public static float operator *(Vector i, Vector j) {//Dot Product
+        public static Vector2 operator *(Vector2 v, float x) {
+            return new Vector2(v.x * x, v.y * x);
+        }
+        public static float operator *(Vector2 i, Vector2 j) {//Dot Product
             return (i.x * j.x) + (i.y * j.y);
         }
-        public static Vector operator /(Vector v, float x) {
-            return new Vector(v.x / x, v.y / x);
+        public static Vector2 operator /(Vector2 v, float x) {
+            return new Vector2(v.x / x, v.y / x);
         }
         /// <summary>
         /// Gets the angle between the two given vectors
         /// </summary>
         /// <returns></returns>
-        public static double AngleBetween(Vector u, Vector v) {
+        public static double AngleBetween(Vector2 u, Vector2 v) {
             return Math.Acos((u * v) / (u.Length * v.Length));
         }
         /// <summary>
         /// Negates the vector
         /// </summary>
-        public void Negate() { this = this * -1; }
+        public void Negate() { /*this.x *= -1; this.y *= -1;*/ this = this * -1; }
         /// <summary>
         /// Normalizes the vector, turning it into a vector in the same direction with a magnitude of one.
         /// </summary>
@@ -66,8 +70,9 @@ namespace Aardvark {
         /// Takes a string in the form "x,y" and converts it to a Vector
         /// </summary>
         /// <param name="s"> The string to construct the vector from.</param>
-        public static Vector Parse(String s) {
-            return new Vector();
+        [System.Obsolete]
+        public static Vector2 Parse(String s) {//TODO
+            return new Vector2();
         }
     } 
 }
---
+++ Vector3.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aardvark {
+    public struct Vector3 {
+        private float x, y, z;
+
+        public Vector3(float i) {
+            this.x = i;
+            this.y = i;
+            this.z = i;
+        }
+        public Vector3(float x, float y, float z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public float X {
+            get { return this.x; }
+            set { this.x=value; }
+        }
+        public float Y {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+        public float Z {
+            get { return this.z; }
+            set { this.z = value; }
+        }
+        public double Length { get { return Math.Sqrt(this.x * this.x + this.y * this.y + this.z * this.z); } }
+        public double LenghtSquared { get { return this.Length * this.Length; } }
+
+        public override string ToString() {
+            return String.Format("<{0},{1},{2}>", this.x, this.y, this.z);
+        }
+
+        public static Vector3 operator +(Vector3 u, Vector3 v) {
+            return new Vector3(u.x + v.x, u.y + v.y, u.z + v.z);
+        }
+        public static Vector3 operator -(Vector3 u, Vector3 v) {
+            v.Negate();
+            return u + v;
+        }
+        public static Vector3 operator *(Vector3 v, float x) {
+            return new Vector3(v.x * x, v.y * x, v.z * x);
+        }
+        public static float operator *(Vector3 v, Vector3 u) {
+            return (v.x * u.x) + (v.y * u.Y) + (v.z * u.z);
+        }
+        public static Vector3 operator /(Vector3 v, float x) {
+            return new Vector3(v.x / x, v.y / x, v.z / x);
+        }
+
+        public static double AngleBetween(Vector3 v, Vector3 u) {
+            return Math.Acos((v * u) / (v.Length * u.Length));
+        }
+
+        public void Negate() {
+            this.x *= -1;
+            this.y *= -1;
+            this.z *= -1;
+        }
+        public void Normalize() { this = this / (float)this.Length; }
+    }
+}
