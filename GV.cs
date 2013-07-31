3702684e58cd0bf00f78792364275fedeab69802
Nikko <nikko.rush@gmail.com>
2013-07-30 22:39:24 -0700

    Added GV class file, first release version

---
+++ GV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aardvark {
+    static class GV {
+        private static List<object> G = new List<object>();
+
+        public static void Add(Object o){
+            G.Add(o);
+        }
+        public static void Remove(Object o) {
+            G.Remove(o);
+        }
+
+        public static List<object> Variables { get { return G; } }
+    }
+}
