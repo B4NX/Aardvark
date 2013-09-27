e04a650e83296eb5d1fbcb1a437b3afccb27b57d
B4NX <nikko.rush@gmail.com>
2013-09-27 08:33:01 -0700

    Started math class, recursive factorial function added.

---
+++ AnimalMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aardvark
+{
+    public static partial class AnimalMath
+    {
+        public static double Factorial(double d)
+        {
+            if (d == 1 || d == 0)
+            {
+                return 1;
+            }
+            return d * Factorial(d - 1);
+        }
+    }
+}
